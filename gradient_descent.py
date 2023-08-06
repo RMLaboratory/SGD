6cc35c5cb8dcaa1b795dee8b38c5bae2856efbfd
cpp <109072496+LEE-nuuks@users.noreply.github.com>
2023-08-06 08:38:45 -0400

    Create gradient_descent.py

---
+++ gradient_descent.py
@@ -0,0 +1,6 @@
+def gradient_descent(gradient, start, learn_rate, n_iter):
+  vector = start
+  for _ in range(n_iter):
+    diff = -learn_rate * gradient(vector)
+    vector += different
+  return vector  
